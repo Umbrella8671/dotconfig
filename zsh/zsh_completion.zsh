f6fef1925604529a825d9b476c6e8399ca6f97fb
cc <umbrella8671@gmail.com>
2025-08-14 23:39:16 +0800

    modify zsh/zsh_completion.zsh, add condition test add fzf.zsh

--- zsh/zsh_completion.zsh
+++ zsh/zsh_completion.zsh
@@ -3,10 +3,13 @@
 # zsh的自动补全与高亮
 autoload -Uz compinit && compinit
 
-source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+[[ -f "/opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh" ]] && source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+[[ -f "/opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]] && source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
-# source $HOME/.config/zsh/completions/uv.zsh
-# source $HOME/.config/zsh/completions/uvx.zsh
-source <(uv generate-shell-completion zsh)
-source <(uvx --generate-shell-completion zsh)
+# [[ -f "$HOME/.config/zsh/completions/uv.zsh" ]] && source $HOME/.config/zsh/completions/uv.zsh
+# [[ -f "$HOME/.config/zsh/completions/uvx.zsh" ]] && source $HOME/.config/zsh/completions/uvx.zsh
+command -v uv &> /dev/null && source <(uv generate-shell-completion zsh)
+command -v uvx &> /dev/null && source <(uvx --generate-shell-completion zsh)
+
+# [[ -f "$HOME/.config/zsh/completions/fzf.zsh" ]] && source $HOME/.config/zsh/completions/fzf.zsh
+command -v fzf &> /dev/null && source <(fzf --zsh)
