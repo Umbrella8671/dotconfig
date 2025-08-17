cff7a03c8ddbe7aafac82b917030b28aeac234cb
cc <umbrella8671@gmail.com>
2025-08-17 18:13:27 +0800

    modify zsh/aliases.zsh, adapt ls to mac and linux

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,11 @@
 # This script file is used for alias
 
-alias ls='ls -G'
-alias ll='ls -lh'
+if [[ "$(uname)" == "Darwin" ]]; then
+  alias ls='ls -G'
+  alias ll='ls -lh'
+else
+  alias ls='ls --color=auto'
+  alias ll='ls -lh'
+fi
+
 alias gitlog='git log --graph --oneline --all --decorate'
